13fca6b187147b57b42faab340e208e0bf6fd5db
João Pedro de Melo França <joaopmfranca@gmail.com>
2024-04-14 11:29:20 -0300

    feat: add currency formatter

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="container max-w-2xl px-6 mx-auto">
       <section className="py-3">
         <small className="text-gray-400 text-md">My Balance</small>
-        <h2 className="text-4xl font-bold">R$ 100 000</h2>
+        <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
       </section>
 
       <section className="flex items-center gap-2 py-3">
---
+++ src/lib/utils.js
@@ -0,0 +1,8 @@
+export const currencyFormatter = (amount) => {
+    const formatter = Intl.NumberFormat("pt-BR", {
+        currency: "BRL",
+        style: "currency",
+    });
+
+    return formatter.format(amount);
+};
