e5a66610b8ac74971f40eea59ba64269648eafa7
joao0814 <joaopmfranca@gmail.com>
2024-04-16 15:00:51 -0300

    style: add z-index

--- src/components/TripModal.jsx
+++ src/components/TripModal.jsx
@@ -1,12 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
-
 const TripModal = ({ isOpen, onClose }) => {
   return (
     <>
       {isOpen && (
-        <div className="absolute top-15 left-0 w-full h-full">
+        <div className="absolute top-15 left-0 w-full h-full z-10 ">
           <div className="container mx-auto max-w-xl h-[60vh] rounded-3xl bg-slate-800 py-6 px-4">
             <button
               className="btn mb-4 font-bold rounded-full bg-slate-600"
