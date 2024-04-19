5b589a947582ba1a87f40705fe29d779717e4b23
João Pedro de Melo França <joaopmfranca@gmail.com>
2024-04-19 13:52:17 -0300

    chore: change language for pt-BR

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ import AuthContextProvider from "@/lib/store/auth-context";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
